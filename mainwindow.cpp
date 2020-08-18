7a6ce076c77ca97a922bb2d5223e420e254987e3
Dmitriy Snitsaruk <norzes@mail.ru>
2020-08-18 11:42:55 +0400

    added basic logic

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,31 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
+
+void MainWindow::on_btPath_clicked() {
+    auto path = QFileDialog::getExistingDirectory();
+    ui->tbPath->setText(path);
+    QDir dir;
+    dir.setPath(path);
+    dir.setNameFilters(QStringList() << "*.cpp" << "*.c" << "*.h" << "*.hpp");
+    QStringList files = dir.entryList();
+    foreach (auto item, files) {
+        ui->listFiles->addItem(item);
+    }
+}
+
+void MainWindow::on_pbFormat_clicked() {
+    QProcess proc;
+    ui->listFiles->setCurrentRow(0);
+    QStringList args;
+    args.append("astyle --options=codeStyleAStyle " + ui->tbPath->text() + ui->listFiles->currentItem()->text());
+    proc.execute("cmd.exe", args);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QStringListModel>
+#include <QProcess>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+        Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+    private slots:
+        void on_btPath_clicked();
+
+        void on_pbFormat_clicked();
+
+    private:
+        Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
