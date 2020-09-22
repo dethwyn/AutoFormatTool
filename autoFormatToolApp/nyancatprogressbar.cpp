d0a40ede9268b59e522e9077aefe2a3ea2b7ab5e
Дмитрий Сницарук <DGSnitsaruk@vniia.net>
2020-09-22 09:18:39 +0400

    Украшение кастомного прогресс-бара

--- autoFormatToolApp/nyancatprogressbar.cpp
+++ autoFormatToolApp/nyancatprogressbar.cpp
@@ -20,6 +20,7 @@ int NyanCatProgressBar::getTypeRay() {
 }
 
 void NyanCatProgressBar::paintEvent(QPaintEvent *) {
+    //
     auto val = value();
     auto pos = QStyle::sliderPositionFromValue(minimum(), maximum(), val, width());
     QPainter painter(this);
@@ -27,20 +28,38 @@ void NyanCatProgressBar::paintEvent(QPaintEvent *) {
     QImage src("1.png");
     auto dest = src.scaled(40, height());
     QColor colors[] = {k_red, k_orange, k_yellow, k_green, k_lightBlue, k_purple};
+    QColor colors_l[] = {k_red_l, k_orange_l, k_yellow_l, k_green_l, k_lightBlue_l, k_purple_l};
+    drawRectBorder(&painter);
+    painter.drawRect(3, 3, width() - 7, height() - 7);
     if(typeRay == 1) {
         int hRect = height() / 6;
-        if(val <= maximum() && val > 0) {
+        if(val < maximum() && val > 0) {
             int i = 0;
+            drawRectBorder(&painter);
             for(auto &item : colors) {
                 painter.setPen(item);
                 painter.setBrush(item);
-                painter.drawRect(pos_draw, i * hRect, pos, hRect);
+                painter.drawRect(pos_draw, i * hRect + 1, pos, hRect);
                 i++;
             }
             painter.drawImage(pos - dest.width() / 2, 0, dest);
+        } else if(val == maximum()) {
+            int i = 0;
+            drawRectBorder(&painter);
+            for(auto &item : colors) {
+                painter.setPen(item);
+                painter.setBrush(item);
+                painter.drawRect(pos_draw, i * hRect + 1, pos - dest.width() / 2, hRect);
+                i++;
+            }
+            painter.drawImage(pos - dest.width(), 0, dest);
+        } else {
+            drawRectBorder(&painter);
         }
     } else {
         if(val <= maximum() && val > 0) {
+            drawRectBorder(&painter);
+            painter.drawRect(3, 3, width() - 7, height() - 7);
             painter.setPen(QColor(127, 127, 127));
             auto *gradient = new QLinearGradient();
             gradient->setStart(0, 0);
@@ -48,8 +67,19 @@ void NyanCatProgressBar::paintEvent(QPaintEvent *) {
             gradient->setColorAt(0.0, QColor(200, 255, 200));
             gradient->setColorAt(1.0, QColor(0, 255, 0));
             painter.setBrush(*gradient);
-            painter.drawRect(pos_draw, -1, pos, height());
+            painter.drawRect(0, 0, pos - 1, height() - 1);
             delete gradient;
         }
     }
+}
+
+void NyanCatProgressBar::drawRectBorder(QPainter *p) {
+    auto gray = QColor::fromRgb(173, 173, 173);
+    auto lightGray = QColor::fromRgb(240, 240, 240);
+    p->setPen(gray);
+    p->setBrush(gray);
+    p->drawRect(0, 0, width(), height());
+    p->setPen(lightGray);
+    p->setBrush(lightGray);
+    p->drawRect(1, 1, width() - 3, height() - 3);
 }
--- autoFormatToolApp/nyancatprogressbar.h
+++ autoFormatToolApp/nyancatprogressbar.h
@@ -18,6 +18,13 @@ const auto k_green = QColor::fromRgb(0, 255, 0);
 const auto k_lightBlue = QColor::fromRgb(0, 176, 255);
 const auto k_purple = QColor::fromRgb(100, 50, 255);
 
+const auto k_red_l = QColor::fromRgb(255, 100, 100);
+const auto k_orange_l = QColor::fromRgb(255, 160, 100);
+const auto k_yellow_l = QColor::fromRgb(255, 255, 100);
+const auto k_green_l = QColor::fromRgb(100, 255, 100);
+const auto k_lightBlue_l = QColor::fromRgb(100, 176, 255);
+const auto k_purple_l = QColor::fromRgb(200, 150, 255);
+
 class NyanCatProgressBar : public QProgressBar {
 public:
     explicit NyanCatProgressBar(QWidget *parent = nullptr);
@@ -26,6 +33,7 @@ public:
 protected:
     void paintEvent(QPaintEvent *) Q_DECL_OVERRIDE;
     int typeRay;
+    void drawRectBorder(QPainter *p);
 };
 
 #endif // NYANCATPROGRESSBAR_H
